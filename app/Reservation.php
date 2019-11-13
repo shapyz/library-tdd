0dbaeb3da1cdd9a57d3066c4e5bcf260c0ba0507
ashiraf jabir <shapynazle@gmail.com>
2019-11-13 08:30:14 +0300

    Adding book reservation

---
+++ app/Reservation.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    /**
+     * Guarded Columns
+     * @var array
+     */
+    protected $guarded = [];
+}
