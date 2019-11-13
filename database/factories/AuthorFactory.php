fa6a315d433f5eea485ee37634711d10d7eea699
ashiraf jabir <shapynazle@gmail.com>
2019-11-13 08:33:04 +0300

    Adding factories

--- database/factories/AuthorFactory.php
+++ database/factories/AuthorFactory.php
@@ -7,6 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Author::class, function (Faker $faker) {
     return [
-        //
+        'name'  =>  $faker->name,
+        'dob'   =>  now()->subYears(18)
     ];
 });
